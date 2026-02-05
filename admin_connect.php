426911320696e10bd20f17b8b5f514a52157989f
Niru Bhandari <nirubhandari04@gmail.com>
2026-02-05 22:48:34 +0545

    all file added

---
+++ admin_connect.php
@@ -0,0 +1,8 @@
+<ul>
+            <li><a href="adminManageUsers.php">Manage Users</a></li>
+            <li><a href="adminManageProducts.php">Manage Products</a></li>
+            <li><a href="adminViewTransactions.php">View Transactions</a></li>
+            <li><a href="adminViewRequests.php">Manage Requests</a></li>
+            <li><a href="adminAnalytics.php">View Reports & Analytics</a></li>
+            <li><a href="adminLogout.php">Logout</a></li>
+        </ul>
